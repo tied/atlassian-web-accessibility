f4efbd43ba9be2d048d19a3848d5653a86d0b393
zlmitche <33164163+zlmitche@users.noreply.github.com>
2018-05-25 13:27:06 -0400

    Modifications

---
+++ confluence-edits/confluence/includes/js/header-user-menu.js
@@ -0,0 +1,28 @@
+/**
+ * JS for initialising the user menu in the header
+ */
+define('confluence/header-user-menu', [
+    'jquery',
+    'confluence/meta',
+    'underscore'
+], function (
+        $,
+        Meta,
+        _
+) {
+    "use strict";
+
+    function initialiseUserAvatar() {
+        var avatarUrl = Meta.get('current-user-avatar-uri-reference');
+        $('#user-menu-link').find('.aui-avatar-inner img').attr('src', avatarUrl).attr('alt', '');
+    }
+
+    return function () {
+        _.defer(initialiseUserAvatar);
+    };
+});
+
+require('confluence/module-exporter').safeRequire('confluence/header-user-menu', function (HeaderUserMenu) {
+    require('ajs').toInit(HeaderUserMenu);
+});
+
