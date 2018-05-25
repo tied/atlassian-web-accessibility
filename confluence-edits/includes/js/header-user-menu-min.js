34de5ca17d0de788d6ca0d7a5d0a57bf962878a7
zlmitche <33164163+zlmitche@users.noreply.github.com>
2018-05-25 13:24:57 -0400

    Original Files

---
+++ confluence-edits/includes/js/header-user-menu-min.js
@@ -0,0 +1,1 @@
+define("confluence/header-user-menu",["jquery","confluence/meta","underscore"],function(a,c,b){function d(){var b=c.get("current-user-avatar-uri-reference");a("#user-menu-link").find(".aui-avatar-inner img").attr("src",b)}return function(){b.defer(d)}});require("confluence/module-exporter").safeRequire("confluence/header-user-menu",function(a){require("ajs").toInit(a)});
