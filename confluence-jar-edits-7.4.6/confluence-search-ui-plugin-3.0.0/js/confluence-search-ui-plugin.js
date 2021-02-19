08fe5f48a9c0a5813f492991f5c9eb926004bccf
bavierte <bavierte@syr.edu>
2021-02-19 10:22:41 -0500

    confluence-search-ui-plugin-3.0.0 init

---
+++ confluence-jar-edits-7.4.6/confluence-search-ui-plugin-3.0.0/js/confluence-search-ui-plugin.js
@@ -0,0 +1,124 @@
+require(['ajs', 'wrm', 'document', 'confluence/api/logger', 'confluence/meta', 'confluence/api/constants', 'confluence/search-ui/analytics-support'],
+    function (ajs, wrm, document, logger, meta, constants, analyticsSupport) {
+        const events = ['click', 'focus'];
+        var searchUI;
+        var searchInput;
+        var spinner;
+        var tooltip;
+        var isTooltipShown = false;
+        var isLoading = false;
+
+        function toggleIcon() {
+            searchUI.classList.toggle("hide-icon");
+        }
+
+        function startLoading() {
+            isLoading = true;
+            toggleIcon();
+            spinner.style.display = 'block';
+        }
+
+        function stopLoading() {
+            isLoading = false;
+            toggleIcon();
+            spinner.style.display = 'none';
+        }
+
+        function unregisterHandlers() {
+            events.forEach(function (event) {
+                searchInput.removeEventListener(event, lazyLoad);
+            });
+        }
+
+        function showTooltip() {
+            if (!isTooltipShown) {
+                tooltip.style.display = 'block';
+                isTooltipShown = true;
+            }
+        }
+
+        function hideTooltip() {
+            if (isTooltipShown) {
+                tooltip.style.display = 'none';
+                isTooltipShown = false;
+            }
+        }
+
+        function handleClickAndFocusEvent(event) {
+            showTooltip();
+            event.stopPropagation();
+        }
+
+        function handleInputEvent(event) {
+            if (event.target.value) {
+                showTooltip();
+            } else {
+                hideTooltip();
+            }
+        }
+
+        function handleKeyDownEvent(event) {
+            const ENTER_KEY_CODE = 13;
+            if (event.keyCode === ENTER_KEY_CODE) {
+                const searchTerm = event.target.value;
+                const cql = searchTerm ? '?cql=&queryString=' + encodeURIComponent(searchTerm) : '';
+                window.location.href = constants.CONTEXT_PATH + '/dosearchsite.action' + cql;
+            }
+        }
+
+        function handleDocumentClick(event) {
+            if (searchUI.contains(event.target)) {
+                showTooltip();
+            } else {
+                hideTooltip();
+            }
+        }
+
+        function lazyLoad() {
+            if (isLoading) return;
+
+            startLoading();
+            wrm
+                .require(['wrc!confluence-search-ui-plugin-main'])
+                .done(function () {
+                    require(['confluence-search-ui-plugin-main'], function (SearchUI) {
+                        SearchUI.init();
+                        unregisterHandlers();
+                        stopLoading();
+                    });
+                })
+                .fail(function () {
+                    stopLoading();
+                    logger.error('fail to load confluence-search-ui-plugin-main');
+                });
+        }
+
+        ajs.toInit(function () {
+            searchUI = document.getElementById('search-ui');
+            // login screen doesn't have navigation search
+            if (!searchUI) {
+                logger.log('no search-ui element found');
+                return;
+            }
+
+            analyticsSupport.init();
+            searchInput = searchUI.querySelector('#quick-search-query');
+            spinner = searchUI.querySelector('aui-spinner');
+            tooltip = searchUI.querySelector('[role=alert]');
+
+            if (!meta.get('global-settings-quick-search-enabled')) {
+                searchInput.addEventListener('keydown', handleKeyDownEvent);
+                searchInput.addEventListener('input', handleInputEvent);
+
+                events.forEach(function (event) {
+                    searchInput.addEventListener(event, handleClickAndFocusEvent);
+                });
+
+                document.addEventListener('click', handleDocumentClick);
+            } else {
+                events.forEach(function (event) {
+                    searchInput.addEventListener(event, lazyLoad);
+                });
+            }
+        });
+    });
