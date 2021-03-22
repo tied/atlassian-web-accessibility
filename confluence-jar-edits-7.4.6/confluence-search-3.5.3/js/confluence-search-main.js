ce9221a9e15ca7d474e1ef18bf6f16bacc3df0e5
bavierte <bavierte@syr.edu>
2021-03-22 16:18:51 -0400

    Removed test 2

--- confluence-jar-edits-7.4.6/confluence-search-3.5.3/js/confluence-search-main.js
+++ confluence-jar-edits-7.4.6/confluence-search-3.5.3/js/confluence-search-main.js
@@ -1,11 +1,3 @@
 require(['jquery', 'confluence-search/confluence-search'], function ($, setupSearch) {
     $(setupSearch);
-
-    /* SU CUSTOM */
-    $('div.cql-filter-field-input-container div ul li a').on('keypress',function(e){
-        var keyCode = e.which;
-        if ((keyCode===13)||(keyCode===32)) {
-            $(this).click();
-        }
-    });
 });
