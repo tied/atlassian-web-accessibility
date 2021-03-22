f62454b51b8b003373c8bbc3430611f01127af07
bavierte <bavierte@syr.edu>
2021-03-22 16:11:46 -0400

    Test fix for keyboard 2

--- confluence-jar-edits-7.4.6/confluence-search-3.5.3/js/confluence-search-main.js
+++ confluence-jar-edits-7.4.6/confluence-search-3.5.3/js/confluence-search-main.js
@@ -1,3 +1,11 @@
 require(['jquery', 'confluence-search/confluence-search'], function ($, setupSearch) {
     $(setupSearch);
+
+    /* SU CUSTOM */
+    $('div.cql-filter-field-input-container div ul li a').on('keypress',function(e){
+        var keyCode = e.which;
+        if ((keyCode===13)||(keyCode===32)) {
+            $(this).click();
+        }
+    });
 });
