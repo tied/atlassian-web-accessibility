956718876aac7adbb1b679f992b4aad62a3d4cb5
bavierte <bavierte@syr.edu>
2021-03-22 11:12:19 -0400

    Cleanup

--- confluence-jar-edits-6.13.4/watch-button-4.0.4/js/notification-min.js
+++
@@ -1,1 +0,0 @@
-define("confluence-watch-button/watch-notification",["jquery","aui/flag"],function(b,a){return function(e){var c=document.getElementById("watch-notification");if(c!=null){c.close()}var d=a({type:"success",body:e,close:"auto"});d.setAttribute("id","watch-notification")}});
